5b61a8425b55914c4b2843cadb75a67fd80aa125
nhocconsr <46308607+nhocconsr@users.noreply.github.com>
2019-01-02 11:03:08 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+$stat_sseth=true;
+$msgsseth= "[---] enjoy your life";
+$stat_cp = false;
+$msgcp = "[--] enjoy your life";
+$figlet= $ijo."
+───────────────────────────────────────────
+── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." [`  ||    _    _.       
+── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
+── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
+  $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
+  $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
+  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo." 73jqem    ".$t;
