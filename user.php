ec529f88a1d73887d43eae238a28e4996871c40a
nhocconsr <46308607+nhocconsr@users.noreply.github.com>
2019-01-12 00:11:41 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,13 +1,55 @@
 <?php
-$stat_sseth=true;
-$msgsseth= "[---] enjoy your life";
-$stat_cp = true;
-$msgcp = "[--] enjoy your life";
+echo $putih."[$red!$putih]$red Warning !!!".$t;
+echo $putih."[$ijo!$putih] Ini adalah program ilegal".$t;
+echo $putih."[$kuning!$putih] Resiko sepenuhnya di tanggung pengguna".$t;
+$msgwe=$putih."[$biru>$putih] Subscribe Channel Cocentz 😊".$t;
+$msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
+$msgtt=$putih."[*] Enjoy Your Life !! ".$t;
+$msgveeu=$putih."[*] Enjoy Your Life !! ".$t;
+$msgads=$putih."[$biru>$putih] Subscribe Channel YT Channel Cocentz & adi bordir 😁".$t;
+$msgssdoge=$putih."[^] Enjoy Your Life !! ".$t;
+$stat_dony=true;
+$st_dony=true;
+$yd_stat=false;
+$vip1=false;
+$weblock=[""];
+$ydblock=[""];
+$we_stat=true;
+$stat_cp= true;
+$msgcp =$putih."[!] enjoy your life";
 $figlet= $ijo."
-───────────────────────────────────────────
-── ▄▀ ▄▀▄ █▀▄ ▀ █▄─█ ▄▀▀ ".$kuning." [`  ||    _    _.       
-── █─ █▀█ █─█ █ █─▀█ █─▀▌".$kuning." | L||| \/(/_|`_\|".$ijo."
-── ─▀ ▀─▀ █▀─ ▀ ▀──▀ ▀▀▀───────────────────".$t.
-  $putih."creator:".$ijo." adidoank   ".$putih."[--]  ide  :".$ijo." sungging  ".$t.
-  $putih."chanel :".$ijo." adi bordir ".$putih."[--] chanel:".$ijo." sungging  ".$t.
-  $putih."invite :".$ijo." ipzme2     ".$putih."[--] invite:".$ijo." 73jqem    ".$t;
+▄████▄   ▄▄▄       ██▓███   ██▓ ███▄    █   ▄████
+▒██▀ ▀█  ▒████▄    ▓██░  ██▒▓██▒ ██ ▀█   █  ██▒ ▀█▒
+▒▓█    ▄ ▒██  ▀█▄  ▓██░ ██▓▒▒██▒▓██  ▀█ ██▒▒██░▄▄▄░
+▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██▄█▓▒ ▒░██░▓██▒  ▐▌██▒░▓█  ██▓
+▒ ▓███▀ ░ ▓█   ▓██▒▒██▒ ░  ░░██░▒██░   ▓██░░▒▓███▀▒
+░ ░▒ ▒  ░ ▒▒   ▓▒█░▒▓▒░ ░  ░░▓  ░ ▒░   ▒ ▒  ░▒   ▒$putih
+[ Creator :$turkis Cocentz$putih | Kode :$turkis 0uhij9$putih |$red v.4.1 Full$putih ]$ijo
+░  ▒     ▒   ▒▒ ░░▒ ░      ▒ ░░ ░░   ░ ▒░  ░   ░
+░          ░   ▒   ░░        ▒ ░   ░   ░ ░ ░ ░   ░
+░ ░            ░  ░          ░           ░       ░
+░".$t;
+$data= [
+ //we
+'Cocentz','alianz.hamza','Hamzah Ismail','Lembu Gaming','Cybertz HackrzAllx','Ozil Oy','Alianz Jr.',
+'Adit yoo','Sunda {G.R.T','Channel Cocentz','Sesha mn','Mas Boot','Dex Zizah','Apriliana Apriliana',
+'[ IRKOP ]','Martus Alae','Taleri Muloie','Lise Martis','Siti Berti','Slamet Fards','Vertiem Lolite',
+'THE JOET','Yogi Chiko','Jon Tendy','Panji Saputra','Michael Hermawan','Amelia Putri','Alexander Axio',
+'Kilaer Kioae','Umam Tuner','Alfa Jon','Jon Aldi','Dewi Septiani','Alex Siregar','Wendii Cimoot','Abdull',
+'Coki','behel.uje10@gmail.com','M.Khoirul Anwar','M. Khoirul Anwar',
+//y
+'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
+'23','24','25','26','27','28','29','30',
+ '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','22',
+ 'Kolek Cau','Melin Syg','Channel Cocentz',
+ //yg
+ 'Oblo Gaming2',
+ //v
+ 'Ozil Oy','Alianz X Cybertz',
+ //t
+ 'Cocentz','Hamzah Ismail','Kolek Cau','Ozil Oy',
+        ];
+$ydnews = ['tes','premium',];
+$watch = ['tes',];
+$veeu = ['tes',];
+$toto = ['tes',];
